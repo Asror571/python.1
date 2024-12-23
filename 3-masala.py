e22932ee8b1b223ceca4f82859429e7d811d6145
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-23 20:47:29 +0500

    3-masala

---
+++ 3-masala.py
@@ -0,0 +1,8 @@
+#Foydalanuvchidan ma'lumotlar olindi
+
+ism = input("What is your name :")
+color = input("How old are you?:")
+
+#Formatlandi va natija chiqarildi 
+
+result = f"Assalomu Alaykum {ism} Your favorite color is{color}"

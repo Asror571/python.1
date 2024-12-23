6bbd338fe1daaca4003b1ccf3ebc44ab77887897
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-23 20:49:15 +0500

    3-masala

--- 3-masala.py
+++ 3-masala.py
@@ -1,8 +1,9 @@
 #Foydalanuvchidan ma'lumotlar olindi
 
 ism = input("What is your name :")
-color = input("How old are you?:")
+color = input("Your favorite color is?:")
 
 #Formatlandi va natija chiqarildi 
-
 result = f"Assalomu Alaykum {ism} Your favorite color is{color}"
+#Natija chiqarildi 
+print(result)

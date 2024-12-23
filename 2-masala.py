78155ede8c98fd05ba6488e01b1e32bff1acd779
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-23 20:16:49 +0500

    2-masala

---
+++ 2-masala.py
@@ -0,0 +1,10 @@
+# O'zgaruvchilar yaratildi va unga qiymat berildi 
+number = 34
+name = "Asror"
+age = 15
+
+#Formatlandi
+result  = f"Assalomu alaykum bratim {name} My age is {age}  My favorite number {number}"
+#Natija chiqarildi 
+
+print(result)

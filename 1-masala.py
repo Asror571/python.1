6c545ebe3b2d3f3e427db4c1b3584390751039ac
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-23 19:46:15 +0500

    1-masala

---
+++ 1-masala.py
@@ -0,0 +1,20 @@
+# O'zgaruvchilar olindi va ularga qiymat berildi 
+
+number = 25
+number2 = 3.14
+word = "Salom, dunyo!"
+condition = True
+
+#O'zgauvchilarni type chiqarildi 
+
+print(type(number))
+print(type(number2))
+print(type(word))
+print(type(condition))
+
+# Eng songi natijalar chiqarildi 
+
+print("My favorite number ",number)
+print("My favorite number two ",number2)
+print("My favorite word ",word)
+

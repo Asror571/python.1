695e719d1811bda630975918af7905bd56c6bafa
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-23 20:06:18 +0500

    1-masala

--- 1-masala.py
+++ 1-masala.py
@@ -12,9 +12,5 @@ print(type(number2))
 print(type(word))
 print(type(condition))
 
-# Eng songi natijalar chiqarildi 
 
-print("My favorite number ",number)
-print("My favorite number two ",number2)
-print("My favorite word ",word)
 

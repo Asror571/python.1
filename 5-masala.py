bac90e3729fd143404946ebe7831a255214dbb46
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-24 09:49:34 +0500

    User kiritgan so'zni 3 marta chiqaradigan dastur

---
+++ 5-masala.py
@@ -0,0 +1,8 @@
+print("Assalomu Alaykum!")
+
+ #Userdan so'zlar olinadi 
+word1 = input("Iltimos  so'z kiriting :")
+
+#Formatlangan xolda 3 marta chiqarildi 
+result = f" { word1 } "
+print(result * 3)

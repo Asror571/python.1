8923e42c5587e50221cf68c8c9df2f7a5f0a20c7
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-24 09:43:57 +0500

    Sonni yig'indisini hamda ko'paytmasini hisoblab beradigan dastur

---
+++ 4-masala.py
@@ -0,0 +1,14 @@
+print("Assalomu Alaykum!")
+
+ #Userdan sonlar olinadi 
+number1 = float(input("Iltimos birinchi sonni kiriting :"))
+number2 = float(input("Iltimos Ikkinchi  sonni kiriting :"))
+
+#Yi'gindisi hisoblandi  
+sum = number1 + number2
+print("Siz kiritgan sonlarni  yig'indisi",sum)
+
+#Ko'paytmasi hisoblandi
+sum = number1 * number2
+print("Siz kiritgan sonlarni  ko'paytmasi",sum)
+

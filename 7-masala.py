9d385f6267e93751cb717e671c3977cbd6ea0d93
Asror571 <asrorqurbonazarov@gmail.com>
2024-12-24 10:10:12 +0500

    User kiritgan sonni butun qismini chiqarib beradigan dastur

---
+++ 7-masala.py
@@ -0,0 +1,9 @@
+print("Assalomu Alaykum!")
+
+ #Userdan sonlar olinadi 
+number1 = int(input("Iltimos birinchi sonni kiriting :"))
+number2 = int(input("Iltimos Ikkinchi  sonni kiriting :"))
+
+#Bo'linmasi  hisoblandi  
+sum = number1 // number2
+print("Siz kiritgan sonlarni  bo'linmasi",sum)
